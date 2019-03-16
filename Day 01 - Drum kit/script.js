0e8e06fb1fa3a634c7bbc4e6365465a94557b182
BColwell131 <bcolwell@tcd.ie>
2019-03-16 16:55:40 +0000

    Day 1 - Drum kit

---
+++ Day 01 - Drum kit/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("keydown", e => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return; //End function
+  audio.currentTime = 0; //Rewind to start (allows for repeat playing)
+  audio.play();
+
+  key.classList.add("playing");
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //Skip if not transform
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+console.log(keys);
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
